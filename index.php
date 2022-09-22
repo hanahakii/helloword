13fdfd3bb3b762a2a34bb9e98e2d4a3d22846b8a
hanahakii <minhndgch200119@fpt.edu.vn>
2022-09-22 16:30:05 +0700

    okok

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php 
-echo "<h1 style = 'color: red'>Hello Word</h1>";
-echo "<h2>Successfully</h2>"; 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VN</title>
+</head>
+
+<body>
+    <h1>Aaaaa</h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/mBr-fluvrgM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    
+    </body>
+
+</html>
